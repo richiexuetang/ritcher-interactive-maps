a12ad37e610853f899b806fb916e71fc65853276
richiexuetang <richard772794659@gmail.com>
2023-06-26 10:52:06 -0700

    feat: add fonts and tiles

--- next.config.js
+++ next.config.js
@@ -3,17 +3,47 @@ const nextConfig = {
   eslint: {
     dirs: ['src'],
   },
-
+  async headers() {
+    return [
+      {
+        source: '/fonts/inter-var-latin.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/HyliaSerifBeta-Regular.otf',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/tw3-Regular.ttf',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
-
-  // Uncoment to add domain whitelist
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //   ],
-  // },
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co*',
+      },
+    ],
+  },
   // SVGR
   webpack(config) {
     config.module.rules.push({
