2f4c5118bbd1476fefb7193ced45aec813b5bff7
richiexuetang <richard772794659@gmail.com>
2023-06-29 23:00:44 -0700

    feat: category group flex change

--- src/components/control/sidebar/CategoryGroup.tsx
+++ src/components/control/sidebar/CategoryGroup.tsx
@@ -45,7 +45,7 @@ const CategoryGroup: React.FC<CategoryGroupPropsType> = ({
 
   return (
     <div className='text-primary-200 flex flex-col py-1 hover:cursor-pointer'>
-      <div className='flex items-center'>
+      <div className='flex flex-wrap items-center'>
         {collapsed ? (
           <FiPlus className='mr-3' onClick={() => setCollapsed(false)} />
         ) : (
@@ -60,37 +60,39 @@ const CategoryGroup: React.FC<CategoryGroupPropsType> = ({
         </div>
       </div>
 
-      {!collapsed &&
-        currentGroup.map((member) => {
-          const hiddenFlag = mapHiddenCategories?.includes(member.categoryId);
-          return (
-            <div
-              key={member.categoryId}
-              className='flex flex-row justify-between'
-              onClick={() => setHide(member.categoryId)}
-            >
-              <NextImage
-                useSkeleton
-                src={`/images/icons/${member.categoryId}.png`}
-                width='25'
-                height='0'
-                alt='Icon'
-                fallbackSrc={`/images/icons/${group}.png`}
-                className='w-auto'
-              />
-              <p className={clsxm([hiddenFlag && 'line-through	'])}>
-                {categoryIdNameMap[member.categoryId]}
-              </p>
-              <p className={clsxm([hiddenFlag && 'line-through	'])}>
-                {(areaConfig.name &&
-                  completedCount[areaConfig?.name] &&
-                  completedCount[areaConfig?.name][member.categoryId]) ||
-                  0}
-                /{categoryCounts[member.categoryId]}
-              </p>
-            </div>
-          );
-        })}
+      <div className='group-container flex flex-wrap'>
+        {!collapsed &&
+          currentGroup.map((member) => {
+            const hiddenFlag = mapHiddenCategories?.includes(member.categoryId);
+            return (
+              <div
+                key={member.categoryId}
+                className='flex flex-row justify-between'
+                onClick={() => setHide(member.categoryId)}
+              >
+                <NextImage
+                  useSkeleton
+                  src={`/images/icons/${member.categoryId}.png`}
+                  width='25'
+                  height='0'
+                  alt='Icon'
+                  fallbackSrc={`/images/icons/${group}.png`}
+                  className='w-auto'
+                />
+                <p className={clsxm([hiddenFlag && 'line-through	'])}>
+                  {categoryIdNameMap[member.categoryId]}
+                </p>
+                <p className={clsxm([hiddenFlag && 'line-through	'])}>
+                  {(areaConfig.name &&
+                    completedCount[areaConfig?.name] &&
+                    completedCount[areaConfig?.name][member.categoryId]) ||
+                    0}
+                  /{categoryCounts[member.categoryId]}
+                </p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
